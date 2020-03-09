c3d6acf1000c3320d6d410abcdb012ada2a8a7a2
逸枫 <15757126126@sina.cn>
2020-03-09 11:21:58 +0800

    完成用户列表功能开发

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
