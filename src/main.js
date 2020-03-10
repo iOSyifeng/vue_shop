831cdb7ee836999385f73246209cd19ca57617e4
逸枫 <15757126126@sina.cn>
2020-03-10 15:39:10 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
@@ -15,6 +16,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
